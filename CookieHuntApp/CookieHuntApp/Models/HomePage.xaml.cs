6b50bb0dc0c2a2957db42589c4551fc79f071874
Niko <nikolaj.mansson@gmail.com>
2019-03-04 11:21:55 +0100

    Added logout

--- CookieHuntApp/CookieHuntApp/Models/HomePage.xaml.cs
+++ CookieHuntApp/CookieHuntApp/Models/HomePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CookieHuntApp.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,5 +17,15 @@ namespace CookieHuntApp.Models
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private void Tblogout_OnClicked(object sender, EventArgs e)
+        {
+            Settings.UserName = "";
+            Settings.Password = "";
+            Settings.Accesstoken = "";
+            Navigation.InsertPageBefore(new SignInPage(), this);
+            Navigation.PopAsync();
+
+        }
+    }
 }
